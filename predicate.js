bc4bc2410b3d43fb6e17640e913e2e1c81bba450
mfrancis95 <mikefrancis95@gmail.com>
2016-03-15 12:18:13 -0400

    Code improvements

--- predicate.js
+++ predicate.js
@@ -1,25 +1,21 @@
-var predicate, callback;
-
-function middleware(request, response, next) {
-    if (predicate(request)) {
-        if (callback && callback(request)) {
+function middleware(predicate, callback) {
+    if (predicate instanceof RegExp) {
+        predicate = regexToPredicate(predicate);
+    }
+    return (request, response, next) => {
+        if (predicate(request)) {
+            if (callback && callback(request)) {
+                next();
+            }
+        }
+        else {
             next();
         }
-    }
-    else {
-        next();
-    }
+    };
 }
 
 function regexToPredicate(regex) {
     return request => request.path.search(regex) >= 0;
 }
 
-module.exports = (predicateFunc, callbackFunc) => {
-    if (predicateFunc instanceof RegExp) {
-        predicateFunc = regexToPredicate(predicateFunc);
-    }
-    predicate = predicateFunc;
-    callback = callbackFunc;
-    return middleware;
-};
+module.exports = middleware;
